25a5166072dd04c850ff3276049f76c4a8c3873f
Matjaž Drolc <mdrolc@gmail.com>
2013-11-16 01:37:58 +0100

    Adding 2D FFT

--- js/fft.js
+++ js/fft.js
@@ -1,19 +1,19 @@
 FFT = function() {
-    var fft_step = function(input_r, input_i, output_r, output_i, offset_in, offset_out, N, s) {
+    var fft_step = function(input_r, input_i, output_r, output_i, offset_in, offset_out, N, s, offset, factor) {
         if(N == 1) {
-            output_r[offset_out] = input_r[offset_in];
-            output_i[offset_out] = input_i[offset_in];
+            output_r[factor*offset_out + offset] = input_r[factor*offset_in + offset];
+            output_i[factor*offset_out + offset] = input_i[factor*offset_in + offset];
         } else {
-            fft_step(input_r, input_i, output_r, output_i, offset_in, offset_out, Math.round(N/2), 2*s);
-            fft_step(input_r, input_i, output_r, output_i, offset_in + s, Math.round(N/2) + offset_out, Math.round(N/2), 2*s);
+            fft_step(input_r, input_i, output_r, output_i, offset_in, offset_out, Math.round(N/2), 2*s, offset, factor);
+            fft_step(input_r, input_i, output_r, output_i, offset_in + s, Math.round(N/2) + offset_out, Math.round(N/2), 2*s, offset, factor);
             for(var k = 0; k < Math.round(N/2); k++) {
-                t_r = output_r[offset_out + k];
-                t_i = output_i[offset_out + k];
-                output_r[offset_out + k] = t_r + Math.cos(-2*Math.PI*k/N) * output_r[k + offset_out + Math.round(N/2)] - Math.sin(-2*Math.PI*k/N) * output_i[k + offset_out + Math.round(N/2)];
-                output_i[offset_out + k] = t_i + Math.sin(-2*Math.PI*k/N) * output_r[k + offset_out + Math.round(N/2)] + Math.cos(-2*Math.PI*k/N) * output_i[k + offset_out + Math.round(N/2)];
-                var prev = output_r[k + offset_out + Math.round(N/2)];
-                output_r[offset_out + k + Math.round(N/2)] = t_r - Math.cos(-2*Math.PI*k/N) * prev + Math.sin(-2*Math.PI*k/N) * output_i[k + offset_out + Math.round(N/2)];
-                output_i[offset_out + k + Math.round(N/2)] = t_i - Math.sin(-2*Math.PI*k/N) * prev - Math.cos(-2*Math.PI*k/N) * output_i[k + offset_out + Math.round(N/2)];
+                t_r = output_r[offset + factor*(offset_out + k)];
+                t_i = output_i[offset + factor*(offset_out + k)];
+                output_r[offset + factor*(offset_out + k)] = t_r + Math.cos(-2*Math.PI*k/N) * output_r[offset + factor*(k + offset_out + Math.round(N/2))] - Math.sin(-2*Math.PI*k/N) * output_i[offset + factor*(k + offset_out + Math.round(N/2))];
+                output_i[offset + factor*(offset_out + k)] = t_i + Math.sin(-2*Math.PI*k/N) * output_r[offset + factor*(k + offset_out + Math.round(N/2))] + Math.cos(-2*Math.PI*k/N) * output_i[offset + factor*(k + offset_out + Math.round(N/2))];
+                var prev = output_r[offset + factor*(k + offset_out + Math.round(N/2))];
+                output_r[offset + factor*(offset_out + k + Math.round(N/2))] = t_r - Math.cos(-2*Math.PI*k/N) * prev + Math.sin(-2*Math.PI*k/N) * output_i[offset + factor*(k + offset_out + Math.round(N/2))];
+                output_i[offset + factor*(offset_out + k + Math.round(N/2))] = t_i - Math.sin(-2*Math.PI*k/N) * prev - Math.cos(-2*Math.PI*k/N) * output_i[offset + factor*(k + offset_out + Math.round(N/2))];
             }
         }
     };
@@ -31,7 +31,7 @@ FFT = function() {
 
         var output_r = new Float32Array(input_r.length);
         var output_i = new Float32Array(input_r.length);
-        fft_step(input_r, input_i, output_r, output_i, 0, 0, input_r.length, 1);
+        fft_step(input_r, input_i, output_r, output_i, 0, 0, input_r.length, 1, 0, 1);
 
         return {r: output_r, i: output_i};
     };
@@ -49,13 +49,39 @@ FFT = function() {
 
         var output_r = new Float32Array(input_r.length);
         var output_i = new Float32Array(input_r.length);
-        fft_step(input_i, input_r, output_i, output_r, 0, 0, input_r.length, 1);
+        fft_step(input_i, input_r, output_i, output_r, 0, 0, input_r.length, 1, 0, 1);
         
+        return {r: output_r, i: output_i};
+    };
+
+    var fft2d = function(input, n1, n2) {
+        var output_r = new Float32Array(n1 * n2);
+        var output_i = new Float32Array(n1 * n2);
+
+        var input_r, input_i;
+        if(input.r == undefined || input.i == undefined) {
+            input_r = input;
+            input_i = new Float32Array(input_r.length);
+        } else {
+            input_r = input.r;
+            input_i = input.i;
+        }
+
+        for(var i = 0; i < n1; i++) {
+            fft_step(input_r, input_i, output_r, output_i, 0, 0, n2, 1, i * n2, 1);
+        }
+        input_r = new Float32Array(output_r);
+        input_i = new Float32Array(output_i);
+        for(var i = 0; i < n2; i++) {
+            fft_step(input_r, input_i, output_r, output_i, 0, 0, n1, 1, i, n1);
+        }
+
         return {r: output_r, i: output_i};
     };
 
     return {
         fft1d: fft1d,
-        ifft1d: ifft1d
+        ifft1d: ifft1d,
+        fft2d: fft2d
     };
 }();
--- js/main.js
+++ js/main.js
@@ -7,6 +7,30 @@ $(document).ready(function(){
         return out;
     };
 
+    var format_image = function(input) {
+        var max = -Number.POSITIVE_INFINITY;
+        var min = Number.POSITIVE_INFINITY;
+        for(var i = 0; i<input.length; i++) {
+            if(input[i]>max) {
+                max = input[i];
+            }
+            if(input[i]<min) {
+                min = input[i];
+            }
+        }
+        var scale = max-min;
+        var d = -min;
+        var out = new Uint8ClampedArray(input.length * 4);
+        for(var i = 0; i<input.length; i++) {
+            var val = Math.round(Math.abs((input[i]+d)*255/scale));
+            out[4*i + 0] = val;
+            out[4*i + 1] = val;
+            out[4*i + 2] = val;
+            out[4*i + 3] = 255;
+        }
+        return out;
+    };
+
     var data = new Float32Array(64);
     for(var i = 0; i<64; i++) {
         data[i] = Math.sin(Math.PI*i/16);
@@ -24,4 +48,20 @@ $(document).ready(function(){
             data: format_data(fft.i, data.length),
             points: { show: false }
         }]);
+
+
+    var canvas = document.getElementById('canvas_');
+    var ctx = canvas.getContext('2d');
+
+    var canvas_in = new Float32Array(512*512);
+    for(var i = 0; i<512; i++) {
+        for(var j = 0; j<512; j++) {
+            canvas_in[i*512 + j] = Math.sin(Math.PI*(2*j*i)/5) + Math.sin(Math.PI*(j+2*i)/15);
+        }
+    }
+    var fft2 = FFT.fft2d(canvas_in, 512, 512);
+
+    var idata = ctx.createImageData(512, 512);
+    idata.data.set(format_image(fft2.i));
+    ctx.putImageData(idata, 0, 0);
 });
